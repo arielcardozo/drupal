6cec919610f0ab67a353e3a18295c4a97810a2ff
Ariel <ariel.card@gmail.com>
2013-09-12 14:03:49 +0200

    varios y modulos de edición

---
+++ sites/all/modules/offers_instruments/music_instruments.sql
@@ -0,0 +1,23 @@
+/*
+-- Query: SELECT * FROM drupal7.instruments
+LIMIT 0, 1000
+
+-- Date: 2013-09-12 10:15
+*/
+INSERT INTO `instruments` (`nid_instrument`,`name_instrument`,`description_instrument`,`general_list`) VALUES (1,'Ninguno','Valor por defecto',1);
+INSERT INTO `instruments` (`nid_instrument`,`name_instrument`,`description_instrument`,`general_list`) VALUES (2,'Trompeta','Instrumento de viento',1);
+INSERT INTO `instruments` (`nid_instrument`,`name_instrument`,`description_instrument`,`general_list`) VALUES (3,'Flauta travesera','Instrumento de viento',1);
+INSERT INTO `instruments` (`nid_instrument`,`name_instrument`,`description_instrument`,`general_list`) VALUES (4,'Violín','Instrumento de cuerda',1);
+INSERT INTO `instruments` (`nid_instrument`,`name_instrument`,`description_instrument`,`general_list`) VALUES (5,'Viola','Instrumento de cuerda',1);
+INSERT INTO `instruments` (`nid_instrument`,`name_instrument`,`description_instrument`,`general_list`) VALUES (6,'Corno ingles y frances','Instrumento de Viento',1);
+INSERT INTO `instruments` (`nid_instrument`,`name_instrument`,`description_instrument`,`general_list`) VALUES (7,'Tuba','Instrumento de Viento',1);
+INSERT INTO `instruments` (`nid_instrument`,`name_instrument`,`description_instrument`,`general_list`) VALUES (8,'Timbales','Instrumento de percusión',1);
+INSERT INTO `instruments` (`nid_instrument`,`name_instrument`,`description_instrument`,`general_list`) VALUES (9,'Redoblante','Instrumento de percusión',1);
+INSERT INTO `instruments` (`nid_instrument`,`name_instrument`,`description_instrument`,`general_list`) VALUES (10,'Bombo','Instrumento de percusión',1);
+INSERT INTO `instruments` (`nid_instrument`,`name_instrument`,`description_instrument`,`general_list`) VALUES (11,'Platillos','Instrumento de percusión',1);
+INSERT INTO `instruments` (`nid_instrument`,`name_instrument`,`description_instrument`,`general_list`) VALUES (12,'Xilófono','Instrumento de percusión',1);
+INSERT INTO `instruments` (`nid_instrument`,`name_instrument`,`description_instrument`,`general_list`) VALUES (13,'Triángulo','Instrumento de percusión',1);
+INSERT INTO `instruments` (`nid_instrument`,`name_instrument`,`description_instrument`,`general_list`) VALUES (14,'Oboe','Instrumento de viento',1);
+INSERT INTO `instruments` (`nid_instrument`,`name_instrument`,`description_instrument`,`general_list`) VALUES (15,'Viloncello','instrumento de viento',1);
+INSERT INTO `instruments` (`nid_instrument`,`name_instrument`,`description_instrument`,`general_list`) VALUES (16,'Piano','instrumento de teclado',1);
+INSERT INTO `instruments` (`nid_instrument`,`name_instrument`,`description_instrument`,`general_list`) VALUES (17,'Compositor','Compositor musical',1);
